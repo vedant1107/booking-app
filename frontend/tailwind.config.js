6d730238caf3228557c51b6e70ae6f23ec3e471d
Vedant Deokar <vedant@eumentis.com>
2024-09-15 09:06:48 +0530

    implemented basic layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    /** overriding default tailwind styles */
+    container: {
+      padding: "10rem",
+    },
   },
   plugins: [],
 };
