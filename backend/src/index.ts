dd377aa01a892f1f6c048db178e2e9e1db560b97
Vedant Deokar <vedant@eumentis.com>
2024-09-15 12:00:53 +0530

    implemented registration and login user functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,10 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth";
+import userRoutes from "./routes/users";
 
+// todo: add try catch for db connection and server listening
 mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
 
 const app = express();
@@ -10,9 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("server is running on localhost:7000");
