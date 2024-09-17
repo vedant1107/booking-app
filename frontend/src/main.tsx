aa0f56df685e2d141f3767d8d518ea6e1e0ca2f7
vedantdeokar <vedantdeokar07@gmail.com>
2024-09-17 11:29:59 +0530

    frontend - cleaned the code for better readability

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContext.tsx";
 import App from "./App.tsx";
 import "./index.css";
-import { AppContextProvider } from "./contexts/AppContext.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
