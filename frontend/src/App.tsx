490ceb43ba490d6f42b887cc10dda215b1ac7d72
vedantdeokar <vedantdeokar07@gmail.com>
2025-01-08 20:06:27 +0530

    frontend | added 'add hotel' functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  // Navigate,
+  Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 function App() {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <>
       <Router>
@@ -45,6 +49,20 @@ function App() {
               </Layout>
             }
           />
+
+          {isLoggedIn && (
+            <>
+              <Route
+                path="/add-hotel"
+                element={
+                  <Layout>
+                    <AddHotel />
+                  </Layout>
+                }
+              />
+            </>
+          )}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </>
