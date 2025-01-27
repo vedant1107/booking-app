26655336a29dee7f2e907d73a446a9267a6b25d8
vedantdeokar <vedantdeokar07@gmail.com>
2025-01-27 11:32:55 +0530

    Added Home page

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -9,6 +9,16 @@ const stripe = new Stripe(process.env.STRIPE_API_KEY as string);
 
 const router = express.Router();
 
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find().sort("-lastUpdated");
+    res.json(hotels);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 router.get("/search", async (req: Request, res: Response) => {
   try {
     const query = constructSearchQuery(req.query);
